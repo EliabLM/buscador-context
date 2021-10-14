3dbf5eaf9209ccb8a1c768585acde98a34e366bd
eliablm <eliablopez@hotmail.com>
2021-10-14 18:36:41 -0500

    Consulta a la api para obtener toda la información sobre una receta con su id

--- src/Context/ModalContext.js
+++ src/Context/ModalContext.js
@@ -6,8 +6,21 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
 	// State del provider
 	const [idreceta, guardarIdReceta] = useState(null);
+	const [receta, guardarReceta] = useState({});
 
-	const url = `www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007`;
+	// Hacer llamado a la api
+	useEffect(() => {
+		const obtenerReceta = async () => {
+			if (!idreceta) return;
+
+			const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${idreceta}`;
+
+			const resultado = await axios.get(url);
+			// console.log(resultado);
+			guardarReceta(resultado);
+		};
+		obtenerReceta();
+	}, [idreceta]);
 
 	return (
 		<ModalContext.Provider value={{ guardarIdReceta }}>
