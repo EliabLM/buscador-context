fe2dd8f6cc9f011c24f7671add7b9f19724dae02
eliablm <eliablopez@hotmail.com>
2021-10-14 19:53:17 -0500

    Agrega modal con material-ui

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,10 +1,31 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { ModalContext } from '../Context/ModalContext';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
+const style = {
+	position: 'absolute',
+	top: '50%',
+	left: '50%',
+	transform: 'translate(-50%, -50%)',
+	width: 600,
+	bgcolor: 'background.paper',
+	border: '2px solid #000',
+	boxShadow: 24,
+	p: 4,
+};
 
 const Receta = ({ receta }) => {
 	// Extraer los valores del context
 	const { guardarIdReceta } = useContext(ModalContext);
-	// console.log(receta);
+
+	// Modal de material ui
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
 	return (
 		<div className="col-md-4 mb-3">
 			<div className="card">
@@ -18,10 +39,31 @@ const Receta = ({ receta }) => {
 					<button
 						className="btn btn-block btn-primary"
 						type="button"
-						onClick={() => guardarIdReceta(receta.idDrink)}
+						onClick={() => {
+							guardarIdReceta(receta.idDrink);
+							handleOpen();
+						}}
 					>
 						Ver Receta
 					</button>
+					<Modal
+						open={open}
+						onClose={() => {
+							handleClose();
+							guardarIdReceta(null);
+						}}
+						aria-labelledby="modal-modal-title"
+						aria-describedby="modal-modal-description"
+					>
+						<Box sx={style}>
+							<Typography id="modal-modal-title" variant="h6" component="h2">
+								Text in a modal
+							</Typography>
+							<Typography id="modal-modal-description" sx={{ mt: 2 }}>
+								Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+							</Typography>
+						</Box>
+					</Modal>
 				</div>
 			</div>
 		</div>
